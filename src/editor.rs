fa383b8ba412ae17f0e6c5cc48dc2c5ccf364a0c
TimuriAlvarez <70894059+TimuriAlvarez@users.noreply.github.com>
2026-06-30 17:01:13 +0300

    More power to editor

--- src/editor.rs
+++ src/editor.rs
@@ -28,16 +28,22 @@ impl<'a> ReBuilder<'a> {
   }
 }
 
-fn replace_all(re: &regex::Regex, haystack: &str, f: impl Fn(&regex::Captures) -> Res::<String>) -> Res::<String> {
+fn replace_all(extract: bool, re: &regex::Regex, haystack: &str, f: impl Fn(&regex::Captures) -> Res::<String>) -> Res::<String> {
+  // Initialize empty string, set the text cursor to the very beginning
   let mut new: String = String::new();
   let mut last_match: usize = 0;
+  // Iterate over all captures
   for caps in re.captures_iter(haystack) {
     let m: regex::Match = caps.get(0).unwrap();
-    new.push_str(&haystack[last_match..m.start()]);
+    // If not extracting: preserve the original data before the first capture and in-between captures
+    new.push_str(if extract { "" } else { &haystack[last_match..m.start()] });
+    // Obtain a replacement for the current capture
     new.push_str(&f(&caps)?);
+    // Set the text cursor after the match
     last_match = m.end();
   }
-  new.push_str(&haystack[last_match..]);
+  // If not extracting: preserve the original data after the last capture
+  new.push_str(if extract { "" } else { &haystack[last_match..] });
   Ok(new)
 }
 
@@ -45,26 +51,31 @@ fn expand_refs_caps(expression_haystack: &String, expression_caps: &regex::Captu
   // Create a regular expression of the reference
   let re: regex::Regex = ReBuilder::Reference.build()?;
   // Replace all occurrences of the reference pattern with the corresponding current capture
-  Ok(replace_all(&re, expression_haystack, |caps: &regex::Captures| -> Res::<String> {
-    // Escape the replacement if necessary
+  Ok(replace_all(false, &re, expression_haystack, |caps: &regex::Captures| -> Res::<String> {
     let result: &str = &expression_caps[caps[1].parse::<usize>()?];
     Ok(if escape {
+      // If the result was obtained from a literal: escape it
       regex::escape(result)
     } else {
+      // Else: leave it as is to prevent unescaping strings that already were unescaped
       result.to_string()
     })
   })?)
 }
 
 fn expand_refs_values(expression_haystack: &String, literals: &[String]) -> Res::<String> {
+  // Bundle literals into a haystack
   let haystack: String = literals.join("\n");
+  // Build a special regex for these literals
   let literals: Vec::<String> = literals.into_iter().map(|literals: &String| regex::escape(literals)).collect();
   let re: regex::Regex = ReBuilder::Literals(&literals).build()?;
+  // Convert literals into captures
   let expression_caps: regex::Captures = re.captures(&haystack).unwrap();
+  // Expand all references from expression haystack to literals' captures
   expand_refs_caps(expression_haystack, &expression_caps, true)
 }
 
-pub fn editor(haystack: &String, pattern: &String, replacement: &String, literals: &[String]) -> Res::<String> {
+pub fn editor(extract: bool, haystack: &String, pattern: &String, replacement: &String, literals: &[String]) -> Res::<String> {
   // Expand all references from the pattern to literals
   let pat: String = expand_refs_values(pattern, literals)?;
   event!(Level::DEBUG, "pattern = {pat:?}");
@@ -75,7 +86,7 @@ pub fn editor(haystack: &String, pattern: &String, replacement: &String, literal
   let rep: String = unescape::unescape(replacement).unwrap();
   event!(Level::DEBUG, "replacement = {rep:?}");
   // Replace all occurrences of the pattern with the replacement
-  Ok(replace_all(&re, haystack, |caps: &regex::Captures| -> Res::<String> {
+  Ok(replace_all(extract, &re, haystack, |caps: &regex::Captures| -> Res::<String> {
     // Expand all references from the replacement to current captures
     event!(Level::DEBUG, "captures = {caps:?}");
     Ok(expand_refs_caps(&rep, caps, false)?)
--- tests/editor.rs
+++ tests/editor.rs
@@ -1,11 +1,11 @@
 use gprl::types::Res;
 
-fn test(input: &str, id: &str, pat: &str, rep: &str, lit: &[&str]) -> Res {
+fn test(input: &str, id: &str, extract: bool, pat: &str, rep: &str, lit: &[&str]) -> Res {
   let haystack: String = std::fs::read_to_string(format!("./tests/data/{input}.txt"))?;
   let pattern: String = pat.to_string();
   let replacement: String = rep.to_string();
   let literals: Vec::<String> = lit.into_iter().map(|lit: &&str| lit.to_string()).collect();
-  let result: String = swiss::editor::editor(&haystack, &pattern, &replacement, &literals)?;
+  let result: String = swiss::editor::editor(extract, &haystack, &pattern, &replacement, &literals)?;
   let expected: String = std::fs::read_to_string(format!("./tests/data/{input}/{id}.txt"))?;
   assert_eq!(result, expected, "{id}");
   Ok(())
@@ -13,41 +13,49 @@ fn test(input: &str, id: &str, pat: &str, rep: &str, lit: &[&str]) -> Res {
 
 #[test]
 fn full_match() -> Res {
-  test("matches", "a1", r"baa aaa", r"ccc ccc", &[])?;
-  test("matches", "a2", r"abb aba", r"ccc ccc", &[])?;
-  test("matches", "a3", r"aab bab", r"ccc ccc", &[])?;
-  test("matches", "a4", r"aab bab\n", r"ccc ccc", &[])?;
-  test("matches", "b1", r"baa aaa", r"ccc\nccc", &[])?;
-  test("matches", "b2", r"abb aba", r"ccc\nccc", &[])?;
-  test("matches", "b3", r"aab bab", r"ccc\nccc", &[])?;
-  test("matches", "b4", r"aab bab\n", r"ccc\nccc", &[])?;
-  test("matches", "c1", r"baa aaa\nabb aba", r"ccc\nccc", &[])?;
-  test("matches", "c2", r"abb aba\naaa bbb", r"ccc\nccc", &[])?;
-  test("matches", "c3", r"bbb abb\naab bab", r"ccc\nccc", &[])?;
-  test("matches", "c4", r"bbb abb\naab bab\n", r"ccc\nccc", &[])?;
+  test("matches", "a1", false, r"baa aaa", r"ccc ccc", &[])?;
+  test("matches", "a2", false, r"abb aba", r"ccc ccc", &[])?;
+  test("matches", "a3", false, r"aab bab", r"ccc ccc", &[])?;
+  test("matches", "a4", false, r"aab bab\n", r"ccc ccc", &[])?;
+  test("matches", "b1", false, r"baa aaa", r"ccc\nccc", &[])?;
+  test("matches", "b2", false, r"abb aba", r"ccc\nccc", &[])?;
+  test("matches", "b3", false, r"aab bab", r"ccc\nccc", &[])?;
+  test("matches", "b4", false, r"aab bab\n", r"ccc\nccc", &[])?;
+  test("matches", "c1", false, r"baa aaa\nabb aba", r"ccc\nccc", &[])?;
+  test("matches", "c2", false, r"abb aba\naaa bbb", r"ccc\nccc", &[])?;
+  test("matches", "c3", false, r"bbb abb\naab bab", r"ccc\nccc", &[])?;
+  test("matches", "c4", false, r"bbb abb\naab bab\n", r"ccc\nccc", &[])?;
   Ok(())
 }
 
 #[test]
 fn partial_match() -> Res {
-  test("matches", "d1", r".*aaa.*", r"ccc ccc", &[])?;
-  test("matches", "d2", r".*aba.*", r"ccc ccc", &[])?;
-  test("matches", "d3", r".*bab.*", r"ccc ccc", &[])?;
-  test("matches", "d4", r".*bab.*\n.*", r"ccc ccc", &[])?;
-  test("matches", "e1", r"(.*)baa(.*)", r"&1ccc&2", &[])?;
-  test("matches", "e2", r"(.*) (.*)", r"&1_&2", &[])?;
-  test("matches", "e3", r"(.*)bab(.*)", r"&1ccc&2", &[])?;
-  test("matches", "e4", r"(.*)bab(.*)\n(.*)", r"&1ccc&2&3", &[])?;
+  test("matches", "d1", false, r".*aaa.*", r"ccc ccc", &[])?;
+  test("matches", "d2", false, r".*aba.*", r"ccc ccc", &[])?;
+  test("matches", "d3", false, r".*bab.*", r"ccc ccc", &[])?;
+  test("matches", "d4", false, r".*bab.*\n.*", r"ccc ccc", &[])?;
+  test("matches", "e1", false, r"(.*)baa(.*)", r"&1ccc&2", &[])?;
+  test("matches", "e2", false, r"(.*) (.*)", r"&1_&2", &[])?;
+  test("matches", "e3", false, r"(.*)bab(.*)", r"&1ccc&2", &[])?;
+  test("matches", "e4", false, r"(.*)bab(.*)\n(.*)", r"&1ccc&2&3", &[])?;
   Ok(())
 }
 
 #[test]
 fn literals() -> Res {
-  test("literals", "a1", r"&1", r"ccc.ccc", &["abc.abc"])?;
-  test("literals", "a2", r"\<&1\>", r"dict", &["val"])?;
-  test("literals", "a3", r"(&1)", r"&1\n&1", &["abc.abc"])?;
-  test("literals", "a4", r"&1.&1", r"&0\n&0", &["abc"])?;
-  test("literals", "a5", r"(&1 =).*", r"&1 255 # swiss &0", &["val.a"])?;
-  test("literals", "a6", r".* # swiss (&1.*)", r"&1", &["val.b"])?;
+  test("literals", "a1", false, r"&1", r"ccc.ccc", &["abc.abc"])?;
+  test("literals", "a2", false, r"\<&1\>", r"dict", &["val"])?;
+  test("literals", "a3", false, r"(&1)", r"&1\n&1", &["abc.abc"])?;
+  test("literals", "a4", false, r"&1.&1", r"&0\n&0", &["abc"])?;
+  test("literals", "a5", false, r"(&1 =).*", r"&1 255 # swiss &0", &["val.a"])?;
+  test("literals", "a6", false, r".* # swiss (&1.*)", r"&1", &["val.b"])?;
+  Ok(())
+}
+
+#[test]
+fn extract() -> Res {
+  test("extract", "a1", true, r"&1", r"&0\n", &["efg"])?;
+  test("extract", "a2", true, r"&1", r"&0\n", &["abcd"])?;
+  test("extract", "a3", true, r"&1(.*)", r"&1\n", &["1234"])?;
   Ok(())
 }
