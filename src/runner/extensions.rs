260eaaa47c9fb72ea6ffea33c73afb9cdf189b05
TimuriAlvarez <70894059+TimuriAlvarez@users.noreply.github.com>
2026-08-28 16:52:00 +0300

    Uniformed regex patterns

--- src/runner/extensions.rs
+++ src/runner/extensions.rs
@@ -1,22 +1,25 @@
-use crate::Res;
+use gprl::types::Res;
 
-mod user_functions {
-  use crate::Res;
-  const MARKER: char = '=';
-  const EXPRESSION: &str = r#""[^"]*"|'[^']*'|[^,]+"#;
+const MARKER: char = '=';
+const ID: &str = r"\<.+\>";
+const BLANK: &str = r"[[:blank:]]";
+const EXPRESSION: &str = r#""[^"]*"|'[^']*'|[^,\n]+"#;
 
-  fn regex_declarations(haystack: &str) -> Res::<String> {
-    let pattern: String = format!(r"^\<(.*)\>\((.*{MARKER}.*)\)(\s?:=\s?.*)$");
+mod optional_arguments {
+  use super::*;
+
+  fn regex_declarations(haystack: &str) -> Res<String> {
+    let pattern: String = format!(r"^({ID})\((.*{MARKER}.*)\)({BLANK}*:={BLANK}*.*)$");
     let replacement: String = format!("&1{MARKER}&2\n");
     crate::editor::editor(true, haystack, &pattern, &replacement, &[])
   }
 
-  fn regex_value(haystack: &str) -> Res::<String> {
+  fn regex_value(haystack: &str) -> Res<String> {
     let pattern: String = format!(r"^({EXPRESSION}).*$");
     crate::editor::editor(true, haystack, &pattern, "&1", &[])
   }
 
-  fn regex_fill_out(haystack: &str, name: &str, argc: usize, value: &str) -> Res::<String> {
+  fn regex_fill_out(haystack: &str, name: &str, argc: usize, value: &str) -> Res<String> {
     let sep: &str = if argc == 0 { "" } else { ", " };
     let argv: String = if argc == 0 { String::new() } else {
       // A pattern that matches either a quoted string (which may contain commas) or an unquoted non-comma sequence
@@ -27,20 +30,20 @@ mod user_functions {
     crate::editor::editor(false, haystack, &pattern, &replacement, &[name.to_string()])
   }
 
-  fn regex_truncate(haystack: &str, name: &str, standard: &str) -> Res::<String> {
-    let pattern: String = format!(r"^\<(&1)\>\(.*\)(\s?:=\s?.*)$");
+  fn regex_truncate(haystack: &str, name: &str, standard: &str) -> Res<String> {
+    let pattern: String = format!(r"^\<(&1)\>\(.*\)({BLANK}*:={BLANK}*.*)$");
     let replacement: String = format!("&1({standard})&2");
     crate::editor::editor(false, haystack, &pattern, &replacement, &[name.to_string()])
   }
 
-  pub fn process_arguments(content: &str) -> Res::<String> {
+  pub fn process(content: &str) -> Res<String> {
     let mut haystack: String = content.to_string();
     // Extract declarations for user defined functions from justfile
-    for old in regex_declarations(&haystack)?.lines() {
+    for declaration in regex_declarations(&haystack)?.lines() {
       // Get function name and it's extended arguments
-      let marker: usize = old.find(MARKER).unwrap();
-      let name: &str = &old[..marker];
-      let extended: &str = &old[marker+1..];
+      let marker: usize = declaration.find(MARKER).unwrap();
+      let name: &str = &declaration[..marker];
+      let extended: &str = &declaration[marker+1..];
       let mut standard: String = extended.to_string();
       // Iterate over arguments to restore it's standard form and fill omitted values out
       loop {
@@ -63,11 +66,10 @@ mod user_functions {
       // Make all declarations justfile-compatible
       haystack = regex_truncate(&haystack, name, &standard)?;
     }
-    // println!("{haystack}");
     Ok(haystack)
   }
 }
 
-pub fn apply(content: &str) -> Res::<String> {
-  user_functions::process_arguments(content)
+pub fn apply(content: &str) -> gprl::types::Res<String> {
+  optional_arguments::process(content)
 }
